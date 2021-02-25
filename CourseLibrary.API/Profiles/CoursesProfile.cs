ab97ad0544a03b063f53ef97b7150a79c5f08857
Daniel Braga <63198961+danieltiagobraga@users.noreply.github.com>
2021-02-25 23:35:51 +0000

    Add
    Add

---
+++ CourseLibrary.API/Profiles/CoursesProfile.cs
@@ -0,0 +1,18 @@
+﻿/*
+    This class is used for creating maps between courses DTOS and Courses models 
+ */
+ using AutoMapper;
+
+namespace CourseLibrary.API.Profiles
+{
+    public class CoursesProfile : Profile
+    {
+        public CoursesProfile()
+        {
+            CreateMap<Entities.Course, Models.CourseDto>();
+            CreateMap<Models.CourseForCreationDto, Entities.Course>();
+            CreateMap<Models.CourseForUpdateDto, Entities.Course>();
+            CreateMap<Entities.Course, Models.CourseForUpdateDto>();
+        }
+    }
+}
---
+++ CourseLibrary.API/ResourceParameters/AuthorsResourceParameters.cs
@@ -0,0 +1,11 @@
+﻿/*
+    This class is used for filtering and searching authors (from querystring parameters)
+ */
+namespace CourseLibrary.API.ResourceParameters
+{
+    public class AuthorsResourceParameters
+    {
+        public string MainCategory { get; set; }
+        public string SearchQuery { get; set; }
+    }
+}
